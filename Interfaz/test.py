5ebf64c11216518708e5e09c2fb8ccac92505d16
Gianfranco_Pallero <gpallero@estudiantesunap.cl>
2023-04-08 13:39:22 -0400

    tkinter

--- Interfaz/test.py
+++ Interfaz/test.py
@@ -1,29 +1,21 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+import tkinter as tk
 
-class MainWindow(QMainWindow):
+class MainWindow(tk.Tk):
     def __init__(self):
         super().__init__()
 
         # Configuración de la ventana principal
-        self.setGeometry(100, 100, 400, 300)
-        self.setWindowTitle('Mi ventana principal')
+        self.geometry('400x300')
+        self.title('Mi ventana principal')
 
         # Creación de un botón
-        self.button = QPushButton('Mi botón', self)
-        self.button.move(150, 150)
-        self.button.clicked.connect(self.on_button_click)
+        self.button = tk.Button(self, text='Mi botón', command=self.on_button_click)
+        self.button.place(relx=0.5, rely=0.5, anchor='center')
 
     def on_button_click(self):
         print('¡El botón fue clickeado!')
 
 if __name__ == '__main__':
-    # Creación de la aplicación
-    app = QApplication(sys.argv)
-
     # Creación de la ventana principal
     main_window = MainWindow()
-    main_window.show()
-
-    # Inicio del bucle de eventos de la aplicación
-    sys.exit(app.exec_())
+    main_window.mainloop()
