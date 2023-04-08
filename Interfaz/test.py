6b9f486371c30e3512f55dc2ee0e844e958c2e7b
Gianfranco_Pallero <gpallero@estudiantesunap.cl>
2023-04-08 17:31:23 -0400

    trying to fix

--- Interfaz/test.py
+++ Interfaz/test.py
@@ -1,6 +1,9 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 import tkinter.filedialog as filedialog
 import os
+import pandas as pd
+
 
 from tkinter import messagebox
 
@@ -38,17 +41,16 @@ class MainWindow(tk.Tk):
         self.horario_label = tk.Label(self.horario_frame, text='Horario de clases', font='Helvetica 16 bold')
         self.horario_label.pack(side=tk.TOP, pady=5)
 
-        self.horario_text = tk.Text(self.horario_frame, height=20, width=80, font='Helvetica 12')
-        self.horario_text.pack(padx=10, pady=10)
+        self.horario_tree = ttk.Treeview(self.horario_frame)
+        self.horario_tree.pack(fill=tk.BOTH, expand=1)
 
         # Botón de carga
-        load_button = tk.Button(self.horario_frame, text='Cargar horario', font='Helvetica 12 bold', command=self.load_horario)
+        load_button = tk.Button(self.horario_frame, text='Cargar horario', font='Helvetica 12 bold', command=lambda: self.load())
         load_button.pack(pady=10)
 
         # Cargar archivo de ejemplo
-        with open(f"{os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir))}\ConexionBDD\horario_ejemplo.txt") as f:
-            horario_text = f.read()
-        self.horario_text.insert('end', horario_text)
+        file_path = os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir, "ConexionBDD", "horario_ejemplo.xlsx"))
+        self.load()
 
     def open_horario_window(self):
         messagebox.showinfo('Horario', 'Abrir ventana de horario')
@@ -63,15 +65,87 @@ class MainWindow(tk.Tk):
         messagebox.showinfo('Salas', 'Abrir ventana de salas')
 
     def open_asignaturas_window(self):
-        messagebox.showinfo('Asignaturas', 'Abrir ventana de asignaturas')
+        # Abrir ventana de asignaturas
+        asignaturas_window = tk.Toplevel(self)
+        asignaturas_window.geometry('400x400')
+        asignaturas_window.title('Asignaturas')
 
-    def load_horario(self):
-        file_path = filedialog.askopenfilename(filetypes=[('Archivo de texto', '*.txt')])
+        # Separador
+        separator = tk.Frame(asignaturas_window, height=2, bd=1, relief=tk.SUNKEN)
+        separator.pack(fill=tk.X, padx=5, pady=5)
+
+        # Centro
+        asignaturas_frame = tk.Frame(asignaturas_window)
+        asignaturas_frame.pack(fill=tk.BOTH, expand=1, padx=10, pady=10)
+
+        asignaturas_label = tk.Label(asignaturas_frame, text='Asignaturas', font='Helvetica 16 bold')
+        asignaturas_label.pack(side=tk.TOP, pady=5)
+
+        # Tabla de asignaturas
+        asignaturas_tree = ttk.Treeview(asignaturas_frame, name='asignaturas_tree')
+        asignaturas_tree.pack(fill=tk.BOTH, expand=1)
+
+        # Cargar archivo de ejemplo
+        file_path = os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir, "ConexionBDD", "asignaturas_ejemplo.xlsx"))
+        asignaturas_df = pd.read_excel(file_path)
+
+        # Convertir el dataframe en una lista de tuplas
+        asignaturas_data = [tuple(x) for x in asignaturas_df.values]
+
+        # Configurar columnas del Treeview
+        asignaturas_tree['columns'] = asignaturas_df.columns
+        for col in asignaturas_df.columns:
+            asignaturas_tree.heading(col, text=col)
+
+        # Insertar datos en el Treeview
+        for row in asignaturas_data:
+            asignaturas_tree.insert("", "end", values=row)
+
+    def load(self):
+        # Cargar archivo de horario
+        file_path = filedialog.askopenfilename(filetypes=[('Archivo de Excel', '*.xlsx')])
         if file_path:
-            with open(file_path) as f:
-                horario_text = f.read()
-            self.horario_text.delete('1.0', 'end')
-            self.horario_text.insert('end', horario_text)
+            # Cargar el archivo excel usando pandas
+            horario_df = pd.read_excel(file_path)
+
+            # Convertir el dataframe en una lista de tuplas
+            horario_data = [tuple(x) for x in horario_df.values]
+
+            # Eliminar cualquier widget existente en el frame de horario
+            for widget in self.horario_frame.winfo_children():
+                widget.destroy()
+
+            # Configurar columnas del Treeview
+            self.horario_tree['columns'] = horario_df.columns
+            for col in horario_df.columns:
+                self.horario_tree.heading(col, text=col)
+
+            # Insertar datos en el Treeview
+            for row in horario_data:
+                self.horario_tree.insert("", "end", values=row)
+
+
+    def load_asignaturas(self):
+        file_path = filedialog.askopenfilename(filetypes=[('Archivo de Excel', '*.xlsx')])
+        if file_path:
+            # Cargar el archivo excel usando pandas
+            asignaturas_df = pd.read_excel(file_path)
+
+            # Convertir el dataframe en una lista de tuplas
+            asignaturas_data = [tuple(x) for x in asignaturas_df.values]
+
+            # Eliminar cualquier widget existente en el frame de asignaturas
+            if hasattr(self, 'horario_tree'):
+                self.horario_tree.destroy()
+
+            # Crear un Treeview para mostrar la tabla
+            asignaturas_tree = ttk.Treeview(self.asignaturas_frame, columns=asignaturas_df.columns, show='headings')
+            for col in asignaturas_df.columns:
+                asignaturas_tree.heading(col, text=col)
+            for row in asignaturas_data:
+                asignaturas_tree.insert("", "end", values=row)
+            asignaturas_tree.pack(fill=tk.BOTH, expand=1)
+
 
 if __name__ == '__main__':
     # Creación de la ventana principal
@@ -79,4 +153,3 @@ if __name__ == '__main__':
 
     # Configuración
     main_window.mainloop()
-
